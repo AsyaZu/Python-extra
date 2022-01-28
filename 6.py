c6f7e258ca5849b1497107ccbd4e24c37b30b257
Asya <nastya.Z1998@yandex.ru>
2022-01-28 11:50:16 +0500

    6

--- 6.py
+++ 6.py
@@ -3,10 +3,10 @@
 # Определите, можно ли таким образом отломить от шоколадки часть, состоящую ровно из k долек. 
 # Программа получает на вход три числа: n, m, k и должна вывести YES или NO.
 
-n = int(input())
-m = int(input())
-k = int(input())
-if k > m or k > n and k < m*n:
-    print('YES')
-else:
-    print('NO')
+def chocolate (n, m, k):
+    answer = 'NO'
+    if ((k>=n or k>=m) and k<m*n) and (k % m == 0 or k % n == 0):
+        answer = 'YES'
+    return answer
+print(chocolate(10, 2, 7))
+    
